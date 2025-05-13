7631255045523237fe7d2654aa3769dc3b08a401
iabhinavrajput <abhinavrajput207@gmail.com>
2025-05-13 11:38:45 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TodoApp(),
+    );
+  }
+}
+
+class TodoApp extends StatefulWidget {
+  const TodoApp({super.key});
+
+  @override
+  State<TodoApp> createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  final List<String> _todos = [];
+  final _controller = TextEditingController();
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
